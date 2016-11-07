16ee5cf5ff31e8e5cbab2aec932000fd4bab8f76
Allan Wamming Lie <allan.wamming.lie@bambora.com>
2016-11-07 09:55:32 +0100

    Fix for paymentrequest only using base storeid

--- app/code/local/Mage/Epay/Block/Adminhtml/Paymentrequest/View.php
+++ app/code/local/Mage/Epay/Block/Adminhtml/Paymentrequest/View.php
@@ -1,37 +1,44 @@
 <?php
-class Mage_Epay_Block_Adminhtml_Paymentrequest_View extends Mage_Adminhtml_Block_Widget_View_Container {
-    public function __construct() {		
-		
+class Mage_Epay_Block_Adminhtml_Paymentrequest_View extends Mage_Adminhtml_Block_Widget_View_Container
+{
+    public function __construct() {
+
 		parent::__construct();
-		
-		$this->_removeButton('edit'); 
-		
+
+		$this->_removeButton('edit');
+
 		$standard = Mage::getModel('epay/standard');
-		
+
 		$this->_headerText = Mage::helper('epay')->__('Payment request');
-		
+
 		$paymentrequest_id = $this->getRequest()->getParam('id');
 		$paymentRequest = Mage::getModel('epay/paymentrequest')->load($paymentrequest_id)->getData();
 
+
+        $order_id = $paymentRequest['orderid'];
+        $order = Mage::getModel('sales/order')->loadByIncrementId($order_id);
+        $store_id = $order->getStoreId();
+
+
 		$soapClient = new SoapClient("https://paymentrequest.api.epay.eu/v1/PaymentRequestSOAP.svc?wsdl");
-		
+
 		$params = array();
-		
+
 		$params["authentication"] = array();
-		$params["authentication"]["merchantnumber"] = $standard->getConfigData('merchantnumber');
-		$params["authentication"]["password"] = $standard->getConfigData('remoteinterfacepassword');
-		
+		$params["authentication"]["merchantnumber"] = $standard->getConfigData('merchantnumber', $store_id);
+		$params["authentication"]["password"] = $standard->getConfigData('remoteinterfacepassword', $store_id);
+
 		$params["paymentrequest"] = array();
 		$params["paymentrequest"]["paymentrequestid"] = $paymentRequest["paymentrequestid"];
-		
+
 		$getPaymentRequest = $soapClient->getpaymentrequest(array('getpaymentrequestrequest' => $params));
-					
+
 		if($getPaymentRequest->getpaymentrequestResult->result)
 		{
 			$this->setPaymentrequestId($paymentRequest["paymentrequestid"]);
 			$this->setPaymentrequest($getPaymentRequest->getpaymentrequestResult);
 		}
-		
+
 		$this->setTemplate('epay/paymentrequest/view.phtml');
     }
 }
